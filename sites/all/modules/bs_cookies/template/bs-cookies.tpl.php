511f7f0df698eabed6d280b22d9a61874428f005
morin.gael <morin.gael@biosample.fr>
2018-07-30 13:06:14 +0200

    prj-cookies : Add cookies manager for user and biosample.

---
+++ sites/all/modules/bs_cookies/template/bs-cookies.tpl.php
@@ -0,0 +1,5 @@
+<div id="bs-cookies-container">
+    <div id="bs-cookies-header"><i class="fa fa-cog" aria-hidden="true"></i><h1>Cookies</h1></div>
+    <div id="bs-cookies-content"><p><?php print($cookies_text); ?></p></div>
+    <div id="bs-cookies-actions"><?php print($form_user_choice); ?></div>
+</div>
